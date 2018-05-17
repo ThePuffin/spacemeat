8beecd4991fbea6d85c20cfd7c4f6978de9cab5f
Fafanback <barre.sylv@gmail.com>
2018-05-17 17:53:46 +0200

    validation de toute les routes

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <h1>Search</h1>
+        );
+    }
+}
+
+export default Search;
