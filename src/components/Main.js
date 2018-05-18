7bd3044cc2bc4ed74310b8dd253f8aa4692e5e2d
ThePuffin <ballutvincent@gmail.com>
2018-05-18 12:07:57 +0200

    bb8

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import MatchUltime from "./MatchUltime";
 import Search from "./Search";
 import ModalOui from "./ModalOui";
 import ModalNon from "./ModalNon";
+import Wait from "./Wait";
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -21,6 +22,7 @@ const Main = () => (
       <Route path="/search" component={Search} />
       <Route path="/modalOui" component={ModalOui} />
       <Route path="/modalNon" component={ModalNon} />
+      <Route path="/wait" component={Wait} />
     </Switch>
   </main>
 );
