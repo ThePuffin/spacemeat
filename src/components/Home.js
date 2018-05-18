b82240a9c468c18e690dfecb15a265a8e18b67c3
Julien Foucher <blomki@gmail.com>
2018-05-18 02:54:46 +0200

    ajout du layout + modifs routes + cartes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import "./Home.css";
+// import { Link } from "react-router-dom";
 
-export default class Example extends React.Component {
+export default class Home extends React.Component {
   render() {
-    return <p>Voilà une jolie homepage</p>;
+    return (
+      <Container id="pagePretendants">
+        <Row className="no-gutters">
+          <Col sm="12">
+            <h1 className="logoHome text-center mt-5">Space Meet</h1>
+            <h2 className="subtitleHome text-center mt-1 mb-5">
+              Fresh meat from the outer space
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
---
+++ src/components/MatchUltime.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./MatchUltime.css";
+import match2 from "./assets/jaba.jpg";
+import MyCard from "./MyCard";
+
+export default class MatchUltime extends React.Component {
+  render() {
+    return (
+      <Container id="pageMatch" className="pageMatch">
+        <Row>
+          <Col>
+            <h1 className="text-center matchTitle pt-4">It's a Match !</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mt-4" xs="6">
+            <MyCard
+              name="Jabba The Hunt"
+              homeworld="Tattooine"
+              cybernetics="Prosthetic right hand"
+              imageUrl="https://starwarsblog.starwars.com/wp-content/uploads/2016/06/jabba-the-hutt-portrait-tall.jpg"
+            />
+          </Col>
+          <Col className="mt-4" xs="6">
+            <MyCard
+              name="Luke Skywalker"
+              homeworld="Tattooine"
+              cybernetics="Prosthetic right hand"
+              imageUrl="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
